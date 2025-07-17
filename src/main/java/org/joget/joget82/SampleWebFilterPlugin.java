5e24a8b7329c06c0de59fcf68823aabfa7b9073f
aqilahani-joget <nor-aqilahani@joget.com>
2025-07-17 16:49:33 +0800

    Add user role selection and use repeater type plugin properties

--- src/main/java/org/joget/joget82/SampleWebFilterPlugin.java
+++ src/main/java/org/joget/joget82/SampleWebFilterPlugin.java
@@ -1,9 +1,7 @@
 package org.joget.joget82;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.net.URL;
 import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,17 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.joget.apps.app.model.PluginWebFilterAbstract;
 import org.joget.apps.app.service.AppUtil;
-import org.joget.commons.util.StringUtil;
-import org.joget.plugin.base.PluginManager;
+import org.joget.commons.util.LogUtil;
+import org.joget.plugin.base.PluginWebSupport;
 import org.joget.plugin.base.SystemConfigurablePlugin;
-import org.joget.workflow.util.WorkflowUtil;
 import org.joget.workflow.model.service.WorkflowUserManager;
+import org.joget.workflow.util.WorkflowUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 
-public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements SystemConfigurablePlugin {
+public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements PluginWebSupport, SystemConfigurablePlugin {
     
     protected Map<String, String> urlMap;
+    protected WorkflowUserManager workflowUserManager;
     
     @Override
     public String getName() {
@@ -32,7 +33,7 @@ public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements Sy
 
     @Override
     public String getVersion() {
-        return "1.0.0";
+        return "8.2.0";
     }
     
     @Override
@@ -52,23 +53,13 @@ public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements Sy
 
     @Override
     public String[] getUrlPatterns() {
-        //return new String[]{"/" + getShortLink() + "/*"};
-        return new String[]{"/rdr/*"};
+        return new String[]{"/api/rdr/*"};
     }
-
-    //public String getShortLink() {
-    //    if (!getPropertyString("shortLink").isEmpty()){
-    //        return getPropertyString("shortLink");
-    //    }
-    //    else {
-    //        return "rdr";
-    //    }
-    //}
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        WorkflowUserManager workflowUserManager = (WorkflowUserManager) AppUtil.getApplicationContext().getBean("workflowUserManager");
-             
+        workflowUserManager = (WorkflowUserManager) AppUtil.getApplicationContext().getBean("workflowUserManager");
+
         //if (!workflowUserManager.isCurrentUserInRole(WorkflowUtil.ROLE_ADMIN)) {
             HttpServletRequest httprequest = (HttpServletRequest) request;
             HttpServletResponse httpresponse = (HttpServletResponse) response;
@@ -81,29 +72,40 @@ public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements Sy
                 savedUrl = httprequest.getHeader("referer");
             }
              
-            if (savedUrl.contains("ulogin")) {
-                savedUrl = savedUrl.replaceAll("ulogin", "userview");
-            }
             String redirectUrl = "";
             String paramValue = "";
             String paramKey = "";
             String requestURI = httprequest.getRequestURI();
             String requestURL = httprequest.getRequestURL().toString();
             boolean redirectFlg = false;
-            Object urlProperty = getProperty("url");
+            Object urlProperty = getProperty("urlRepeater");
             if (urlProperty != null) {
                 for (Object url : (Object[]) urlProperty) {
                     urlMap = ((Map)url);
                     redirectUrl = (String) urlMap.get("redirectUrl");
                     String matchUrl = (String) urlMap.get("matchUrl");
-                    if (requestURI.equals(matchUrl) && !requestURL.equals(redirectUrl)) {
-                        redirectFlg = true;
+                    String[] enabledRoles = (String[]) urlMap.get("enabledRoles").split(";");
+                    String params = "";
+                    if (requestURI.equals(matchUrl) && !requestURL.equals(redirectUrl) && enabledRoles.length > 0) {
+                        if (roleMatchesAny(enabledRoles)) {
+                            redirectFlg = true;
+                        }
                         if (((String) urlMap.get("passParameter")).equals("true")) {
-                            paramKey = (String) urlMap.get("redirectParameter");
-                            paramValue = httprequest.getParameter((String) urlMap.get("matchParameter"));
-                            if (paramValue != null && !paramValue.isEmpty()) {
-                                redirectUrl += ("?" + paramKey + "=" + paramValue); 
+                            Object gridParam = urlMap.get("gridParameter");
+                            for (Object param : (Object[]) gridParam) {
+                                Map<String, String> gridMap = ((Map)param);
+                                paramKey = (String) gridMap.get("redirectParameter");
+                                paramValue = httprequest.getParameter((String) gridMap.get("matchParameter"));
+                                if (paramValue != null && !paramValue.isEmpty()) {
+                                    if (params.isEmpty()) {
+                                        params += ("?" + paramKey + "=" + paramValue);
+                                    }
+                                    else {
+                                        params += ("&" + paramKey + "=" + paramValue);
+                                    }
+                                }
                             }
+                            redirectUrl  += params;
                         }
                         break;
                     }
@@ -128,11 +130,102 @@ public class SampleWebFilterPlugin extends PluginWebFilterAbstract implements Sy
                 
                 //httpresponse.getWriter().write(html);
                 //httpresponse.setContentType("text/html;charset=UTF-8");
-                httpresponse.sendRedirect(redirectUrl);
+                if (isForward(redirectUrl, httprequest)) {
+                    LogUtil.info(getClassName(), "is forwarded to " + redirectUrl);
+                    String forwardUrl = redirectUrl.substring(httprequest.getContextPath().length());
+                    httprequest.getRequestDispatcher(forwardUrl).forward(httprequest, httpresponse);
+                } else {
+                    if (!redirectUrl.startsWith("http://") && !redirectUrl.startsWith("https://")) {
+                        String scheme = httprequest.getScheme();
+                        redirectUrl = scheme + "://" + redirectUrl;
+                    }
+                    LogUtil.info(getClassName(), "is redirected to " + redirectUrl);
+                    httpresponse.sendRedirect(redirectUrl);
+                }
                 return;
             }
         //}
          
         filterChain.doFilter(request, response);
     }
+
+    @Override
+    public void webService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isAdmin = WorkflowUtil.isCurrentUserInRole(WorkflowUserManager.ROLE_ADMIN);
+        if (!isAdmin) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        
+        String action = request.getParameter("action");
+        
+        if ("getRoles".equals(action)) {
+
+            try {
+                JSONArray jsonArray = new JSONArray();
+
+                workflowUserManager = (WorkflowUserManager) AppUtil.getApplicationContext().getBean("workflowUserManager");
+                
+                jsonArray.put(new JSONObject().put("value", workflowUserManager.ROLE_ADMIN).put("label", "Admin"));
+                jsonArray.put(new JSONObject().put("value", workflowUserManager.ROLE_USER).put("label", "Logged in user"));
+                jsonArray.put(new JSONObject().put("value", workflowUserManager.ROLE_ANONYMOUS).put("label", "Anonymous"));
+
+                jsonArray.write(response.getWriter());
+            } catch (Exception ex) {
+                LogUtil.error(this.getClass().getName(), ex, "Get roles options Error!");
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        }
+    }
+    
+    /**
+     * check if link is internal URL
+     * @param redirectUrl
+     * @param request
+     * @return
+     */
+    private boolean isForward(String redirectUrl, ServletRequest request) {
+        try {
+            // 1. Relative links are internal
+            if (!redirectUrl.matches("^(?i)(http|https)://.*")) {
+                // But filter out bare domains like "www.google.com"
+                // Looks like a domain, treat as external
+                // e.g. "/page", "dashboard", "?q=1"
+                
+                return !redirectUrl.matches("^[\\w.-]+\\.[a-z]{2,}.*"); 
+            }
+
+            // 2. Parse the URL
+            URL url = new URL(redirectUrl);
+            String urlHost = url.getHost();
+            int urlPort = url.getPort() != -1 ? url.getPort() : url.getDefaultPort();
+
+            // 3. Get the current request host and port
+            String requestHost = request.getServerName();
+            int requestPort = request.getServerPort();
+
+            // 4. Compare host (and optionally port)
+            return urlHost.equalsIgnoreCase(requestHost) && urlPort == requestPort;
+
+        } catch (Exception e) {
+            // Invalid URL or something went wrong
+            return false;
+        }
+    }
+
+    /**
+     * Check if selected roles correspond to current user role
+     * @param rolesEnabled selected roles
+     * @return
+     */
+    private boolean roleMatchesAny(String[] rolesEnabled) {
+        for (String role : rolesEnabled) {
+            if ((WorkflowUtil.isCurrentUserAnonymous() && WorkflowUtil.getCurrentUsername().equals(role)) || workflowUserManager.isCurrentUserInRole(role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
